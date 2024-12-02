e758caf7a48bf882c5c0a466edfab6289f75e064
Hspsr <119581613+Hspsr@users.noreply.github.com>
2024-12-03 00:19:32 +0100

    Update ActiveHandlers.java

--- app/src/main/java/utb/fai/ActiveHandlers.java
+++ app/src/main/java/utb/fai/ActiveHandlers.java
@@ -6,6 +6,11 @@ public class ActiveHandlers {
     private static final long serialVersionUID = 1L;
     private HashSet<SocketHandler> activeHandlersSet = new HashSet<SocketHandler>();
 
+    void sendMessageToSelf(SocketHandler sender, String message) {
+        if (!sender.messages.offer(message))
+            System.err.printf("Client %s message queue is full, dropping the message!\n", sender.clientID);
+    }
+
     /**
      * sendMessageToAll - Pole zprávu vem aktivním klientùm kromì sebe sama
      * 
@@ -14,10 +19,30 @@ public class ActiveHandlers {
      */
     synchronized void sendMessageToAll(SocketHandler sender, String message) {
         for (SocketHandler handler : activeHandlersSet) // pro vechny aktivní handlery
-            if (handler != sender) {
+            if (handler != sender & !Collections.disjoint(sender.rooms, handler.rooms)) {
                 if (!handler.messages.offer(message)) // zkus pøidat zprávu do fronty jeho zpráv
                     System.err.printf("Client %s message queue is full, dropping the message!\n", handler.clientID);
             }
+    }
+
+    synchronized boolean sendMessageToName(String message, String name) {
+        for (SocketHandler handler : activeHandlersSet) {
+            if (handler.name.equals(name)) {
+                if (!handler.messages.offer(message))
+                    System.err.printf("Client %s message queue is full, dropping the message!\n", handler.clientID);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    synchronized boolean isNameTaken(String name) {
+        for (SocketHandler handler : activeHandlersSet) {
+            if (name.equals(handler.name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
